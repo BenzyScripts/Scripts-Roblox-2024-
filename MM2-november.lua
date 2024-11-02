436b184a05ecf5ccb23266a47e68aa0827038ae4
@WesleyzinXk <153573578+Wesleyzinx@users.noreply.github.com>
2024-11-01 23:47:31 -0300

    Create MM2-november.lua

---
+++ MM2-november.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoTwistedHere/Roblox/main/AntiAFK.lua"))()
